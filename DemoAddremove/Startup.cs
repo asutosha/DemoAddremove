383c380dd18ce9f521109a4be021d9552778fe6a
Asutosha <Asutosha@IBM422-PC0CQCG2>
2017-02-08 10:08:19 +0530

    Add project files.

---
+++ DemoAddremove/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoAddremove.Startup))]
+namespace DemoAddremove
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
